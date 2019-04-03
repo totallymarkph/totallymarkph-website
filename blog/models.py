ed555255011ff99e30191fea9f92fc8085619ae8
mdodong <mdodong@gmail.com>
2019-04-04 00:03:35 +0800

    added basic data model for blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Blog(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    date_created = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
